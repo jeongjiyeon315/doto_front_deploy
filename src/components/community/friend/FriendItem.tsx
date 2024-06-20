965fabc669a3145214d89e0685fa0041f4666067
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-06-20 11:29:43 +0000

    Update Project

--- src/components/community/friend/FriendItem.tsx
+++ src/components/community/friend/FriendItem.tsx
@@ -1,37 +1,57 @@
 import { CharacterProfileIcon } from '@/assets/svg/community';
 import { IMember } from './FriendConstant';
-
+import { useState } from 'react';
+import { STATUS } from './FriendConstant';
+import Button from './Button';
 interface IFriendItemProps {
   member: IMember;
-  isFriend: boolean;
 }
 
-const FriendItem = ({ member, isFriend }: IFriendItemProps) => {
+const FriendItem = ({ member }: IFriendItemProps) => {
+  const [status, setStatus] = useState(member.status);
+
+  const handleDeleteFriendBtn = () => {
+    //TODO:친구삭제 모달 등장
+  };
+
+  const handleFriendBtn = () => {
+    // TODO:친구신청 api요청
+    setStatus(STATUS.WAIT);
+    console.log(member.status);
+  };
+
+  const handleWaitBtn = () => {
+    // TODO:친구신청 취소 api요청
+    setStatus(STATUS.UNFOLLOW);
+    console.log(member.status);
+  };
+  const handleBlockBtn = () => {
+    //TODO:차단 모달 등장
+  };
   return (
-    <li className="flex gap-4 items-center">
-      <CharacterProfileIcon width="40" height="40" />
+    <li className="flex dt:gap-[1rem] items-center mb:justify-between mb:w-full h-[2.5rem]">
+      <CharacterProfileIcon width="2.5rem" height="2.5rem" />
       <span className="text-Light_CategoryText_Icon_Contents text-base dark:text-Dark_CategoryText_Icon">
         {member.nickname}
       </span>
-      {isFriend ? (
-        <>
-          <button className="w-[4.375rem] h-[1.5625rem] bg-Light_CategoryText_Icon_Contents text-Light_Layout-400 text-[0.625rem] rounded-[0.6875rem] dark:text-Dark_CategoryText_Icon">
-            친구삭제
-          </button>
-          <button className="w-[4.375rem] h-[1.5625rem] bg-Light_Layout-300 text-[0.625rem] rounded-[0.6875rem] text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
-            취소
-          </button>
-        </>
+      {status === STATUS.FRIEND ? (
+        <Button color="dark" onClick={handleDeleteFriendBtn}>
+          친구삭제
+        </Button>
+      ) : status === STATUS.UNFOLLOW ? (
+        <Button color="blue" onClick={handleFriendBtn}>
+          친구신청
+        </Button>
       ) : (
-        <>
-          <button className="w-[4.375rem] h-[1.5625rem] bg-Button text-Light_Layout-400 text-[0.625rem] rounded-[0.6875rem] dark:text-Dark_CategoryText_Icon">
-            친구신청
-          </button>
-          <button className="w-[4.375rem] h-[1.5625rem] bg-Light_Layout-100 text-[0.625rem] rounded-[0.6875rem] text-Light_Text_AboutMe dark:text-Dark_Text_AboutMe">
-            차단
-          </button>
-        </>
+        status === STATUS.WAIT && (
+          <Button color="dark" onClick={handleWaitBtn}>
+            대기중
+          </Button>
+        )
       )}
+      <Button color="gray" onClick={handleBlockBtn}>
+        차단
+      </Button>
     </li>
   );
 };
--- src/components/community/friend/FriendList.tsx
+++ src/components/community/friend/FriendList.tsx
@@ -1,4 +1,5 @@
-import { FRIEND_LIST, IMember } from './FriendConstant';
+import { IMember } from './FriendConstant';
+import { FRIEND_LIST } from '@/dummy/Friend';
 import FriendItem from './FriendItem';
 import { useState, useEffect } from 'react';
 const Friends = () => {
@@ -7,11 +8,11 @@ const Friends = () => {
     setFriends(FRIEND_LIST);
   }, []);
   return (
-    <section className="flex flex-col gap-6 w-[25.6875rem] items-center">
+    <section className="flex flex-col gap-6 w-[25.6875rem] mb:w-full items-center">
       <h2 className="text-[1.625rem] text-Light_CategoryText_Icon_Contents">친구 목록</h2>
-      <div className="w-[24rem] h-[28rem] rounded-[1.875rem] bg-Light_Layout-400 flex flex-col items-center px-8 py-[1.1875rem] gap-4 overflow-y-auto scrollbar-hide dark:bg-Dark_Layout-400">
+      <div className="w-[24rem] mb:h-full mb:w-full h-[28rem] rounded-[1.875rem] mb:bg-Light_Layout-200 bg-Light_Layout-400 flex flex-col items-center px-8 py-[1.1875rem] gap-4 overflow-y-auto scrollbar-hide dark:bg-Dark_Layout-400">
         {friends.map((friend) => (
-          <FriendItem key={friend.memberId} member={friend} isFriend={true} />
+          <FriendItem key={friend.memberId} member={friend} />
         ))}
       </div>
     </section>
